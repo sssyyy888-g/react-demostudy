deb2630cd2c0f747588c9f783fd65b22c80bb0e5
Sini(宋颖) <sini.song@yeahmobi.com>
2025-05-27 16:24:07 +0800

    普通css

---
+++ src/02_普通CSS写法/App.jsx
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react'
+import './App.css'
+export class App extends Component {
+ 
+  render() {
+    return (
+      <div>App
+        <h1 className='title'>Hello</h1>
+        <h2 className='content'>Welcome</h2>
+        <h3>Good Morning</h3>
+      </div>
+    )
+  }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './01_内联样式CSS/App';
+// import App from './01_内联样式CSS/App';
+import App from './02_普通CSS写法/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
